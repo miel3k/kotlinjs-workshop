4f1670317314f1b7b38a055e9dbd3a534b1961ba
miel3k <kuba.mielczarek96@icloud.com>
2020-04-26 15:42:35 +0200

    Get users from github api

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,38 @@
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLImageElement
+import org.w3c.xhr.XMLHttpRequest
 import kotlin.browser.document
 
 fun main() {
-    println("Hello from console!")
-    document.write("Hello from browser!")
+    val content = document.getElementById("content") as HTMLDivElement
+    getAsync("https://api.github.com/users") { responseText ->
+        val users = JSON.parse<Array<User>>(responseText)
+        users.forEach {
+            val containerElement = document.createElement("div") as HTMLDivElement
+            val idElement = document.createElement("div") as HTMLDivElement
+            val loginElement = document.createElement("div") as HTMLDivElement
+            val avatarElement = document.createElement("img") as HTMLImageElement
+            idElement.innerHTML = it.id.toString()
+            loginElement.innerHTML = it.login
+            avatarElement.src = it.avatar_url
+            containerElement.appendChild(idElement)
+            containerElement.appendChild(loginElement)
+            containerElement.appendChild(loginElement)
+            containerElement.appendChild(avatarElement)
+            content.appendChild(containerElement)
+        }
+    }
+}
+
+data class User(val id: Int, val login: String, val avatar_url: String)
+
+private fun getAsync(url: String, callback: (String) -> Unit) {
+    val xmlHttpRequest = XMLHttpRequest()
+    xmlHttpRequest.open("GET", url)
+    xmlHttpRequest.onload = {
+        if (xmlHttpRequest.status == 200.toShort()) {
+            callback.invoke(xmlHttpRequest.responseText)
+        }
+    }
+    xmlHttpRequest.send()
 }
