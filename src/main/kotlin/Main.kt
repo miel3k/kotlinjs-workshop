37a869fa34eed96253387f92a6ee227645993ec7
miel3k <kuba.mielczarek96@icloud.com>
2020-04-26 16:29:48 +0200

    Use kotlinx html

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
+import kotlinx.html.dom.append
+import kotlinx.html.img
+import kotlinx.html.js.div
+import kotlinx.html.p
 import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.HTMLImageElement
 import org.w3c.xhr.XMLHttpRequest
 import kotlin.browser.document
 
@@ -8,23 +11,21 @@ fun main() {
     getAsync("https://api.github.com/users") { responseText ->
         val users = JSON.parse<Array<User>>(responseText)
         users.forEach {
-            val containerElement = document.createElement("div") as HTMLDivElement
-            val idElement = document.createElement("div") as HTMLDivElement
-            val loginElement = document.createElement("div") as HTMLDivElement
-            val avatarElement = document.createElement("img") as HTMLImageElement
-            idElement.innerHTML = it.id.toString()
-            loginElement.innerHTML = it.login
-            avatarElement.src = it.avatar_url
-            containerElement.appendChild(idElement)
-            containerElement.appendChild(loginElement)
-            containerElement.appendChild(loginElement)
-            containerElement.appendChild(avatarElement)
-            content.appendChild(containerElement)
+            content.append {
+                div {
+                    p {
+                        +it.login
+                    }
+                    img {
+                        src = it.avatar_url
+                    }
+                }
+            }
         }
     }
 }
 
-data class User(val id: Int, val login: String, val avatar_url: String)
+data class User(val login: String, val avatar_url: String)
 
 private fun getAsync(url: String, callback: (String) -> Unit) {
     val xmlHttpRequest = XMLHttpRequest()
